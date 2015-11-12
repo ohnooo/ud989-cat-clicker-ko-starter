b4105d37cc91a16a5cf3e504b7a0d25b12e29fae
ohnooo <eric.h.1688@gmail.com>
2015-11-12 15:58:48 +0800

    ko  with binding
    with binding,
    
    context binding $parent

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
-var Cat = function(){
-	this.clickCount = ko.observable(0);
-	console.log(this.clickCount());
+var Cat = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	//console.log(this.clickCount());
 
 	this.catLevel = ko.computed(function(){
 		var title,
@@ -19,14 +19,13 @@ var Cat = function(){
 		return title;
 	}, this);// why do I need 'this' keyword?  If I don't use 'this' returns undefine...
 
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-	this.imgAttribution = ko.observable('Someone else image, I am just borrowing for this project');
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
 
 	// Control flow
-	this.nickname = ko.observableArray(
-		[ 'Toby', 'Timmy', 'Tom', 'Tomy']
-	);
+	this.nickname = ko.observableArray(data.nickNames);
+
 	/*
 	this.nickname = ko.observableArray([
 		{name: 'Toby'},
@@ -37,11 +36,20 @@ var Cat = function(){
 }
 
 var ViewModel = function(){
+	// self represent the view model
+	var self = this;
 
-	this.currentCat = ko.observable(new Cat());
+	this.currentCat = ko.observable(new Cat({
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		imgAttribution : 'Someone else image, I am just borrowing for this project',
+		nickNames: ['Toby', 'Timmy', 'Tom', 'Tomy']
+	})	);
 
 	this.incrementCounter = function(){
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		//console.log('incrementCounter is clicked'+ this.currentCat());
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
 
 
