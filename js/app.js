df8cd44e1ae0a3aae5215907775ee89b145daf57
ohnooo <eric.h.1688@gmail.com>
2015-11-12 11:34:30 +0800

    Cat object class
    Move cat related object to a Class cat object.
    
    In the view model add

--- js/app.js
+++ js/app.js
@@ -1,34 +1,27 @@
-var changeLevel = function(numClicks){
 
-	//console.log('changeLevel called ' + numClicks);
-	if(numClicks >= 0 && numClicks <=10){
-		return 'infint';
-	}else if(numClicks > 10 && numClicks <=20){
-		return 'baby';
-	}else if(numClicks >20){
-		return 'teen';
-	}else{
-		return 'undefined';
-	};
-
-};
-
-var ViewModel = function(){
+var Cat = function(){
 	this.clickCount = ko.observable(0);
 	console.log(this.clickCount());
 
-
 	this.catLevel = ko.computed(function(){
-		return changeLevel(this.clickCount());
+		var title,
+			clicks = this.clickCount();
+
+			if(clicks <= 10){
+				title = 'infint';
+			}else if(clicks <= 20){
+				title = 'baby';
+			}else if(clicks > 20){
+				title = 'teen';
+			}else{
+				title = 'undefined';
+			};
+		return title;
 	}, this);// why do I need 'this' keyword?  If I don't use 'this' returns undefine...
 
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
 	this.imgAttribution = ko.observable('Someone else image, I am just borrowing for this project');
-
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
-	};
 
 	// Control flow
 	this.nickname = ko.observableArray(
@@ -41,6 +34,17 @@ var ViewModel = function(){
 		{name: 'Timmy'}
 	]);
 	*/
+}
+
+var ViewModel = function(){
+
+	this.currentCat = ko.observable(new Cat());
+
+	this.incrementCounter = function(){
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+
+
 };
 
 ko.applyBindings(new ViewModel);
